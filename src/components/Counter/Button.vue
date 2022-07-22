9c8b2c1ee710d41cf5a24d8bc0f5121781e4ca3f
spurs0929 <briankang0929@gmail.com>
2022-07-22 19:54:24 +0800

    fix error

---
+++ src/components/Counter/Button.vue
@@ -0,0 +1,30 @@
+<template>
+  <button
+    class="counter-btn"
+    :action="action"
+    @click="count"
+  >{{innerText}}</button>
+</template>
+
+<script>
+export default {
+  name: 'CounterButton',
+  props: {
+    innerText: String,
+    action: String
+  },
+  methods: {
+    count(){
+      this.$emit('dispatch', this.action);
+    }
+  }
+}
+</script>
+
+<style>
+  .counter-btn{
+    width: 60px;
+    height: 40px;
+    margin: 5px;
+  }
+</style>
---
+++ src/components/Counter/Result.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>{{ result }}</h1>  
+</template>
+
+<script>
+export default {
+  name: 'CounterResult',
+  props: {
+    result: Number
+  }
+}
+</script>
+
+<style>
+
+</style>
