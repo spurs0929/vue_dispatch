5d4b40ea29b4f70dbee761ca52d8d3fbd41575df
spurs0929 <briankang0929@gmail.com>
2022-07-22 19:53:06 +0800

    引入counter組件

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="app">
+    <counter />
   </div>
 </template>
 
 <script>
-
+import Counter from './components/Counter';
 
 export default {
   name: 'App',
   components: {
+    Counter
   }
 }
 </script>
