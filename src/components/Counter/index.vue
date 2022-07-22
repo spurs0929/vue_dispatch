a59dc9209e92bbc84974be3a6b816530ddd3f51c
spurs0929 <briankang0929@gmail.com>
2022-07-22 19:56:51 +0800

    引入dispatch修改程式

---
+++ src/components/Counter/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <!-- 結果 -->
+    <counter-result :result="result" />
+    <!-- 按鈕 -->
+    <div>
+      <counter-button
+        innerText="+"
+        action="PLUS"
+        @dispatch="dispatch"
+      />
+      <counter-button
+        innerText="-"
+        action="MINUS"
+        @dispatch="dispatch"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+  import CounterButton from './Button.vue';
+  import CounterResult from './Result.vue';
+  import dispatch from './dispatchers/counter';
+
+  export default {
+    name: 'Counter',
+    components: { 
+      CounterButton,
+      CounterResult
+    },
+    data(){
+      return {
+        result: 0
+      }
+    },
+    methods: {
+      dispatch(...args){
+        dispatch(this)(...args);  
+      }
+    }
+  } 
+</script>
+
+<style>
+
+</style>
